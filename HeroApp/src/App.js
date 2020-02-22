5f6721ea5d3c18ec043c32df58459acd9997e9a7
Matheus WErneck <werneckmatheus12@gmail.com>
2020-02-22 20:11:16 -0300

    Initializing the repository , and react app created

---
+++ HeroApp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import MainNavbar from './components/MainNavbar'
+function App() {
+  return (
+    <div className="App">
+        <MainNavbar/>
+    </div>
+  );
+}
+
+export default App;
